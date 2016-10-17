24979be1041eafbc9d228b273bdd8235f34f69f8
goldsborough <peter@goldsborough.me>
2016-10-17 13:32:08 +0100

    Fixed situation where concatenated and minified files would have multiple license headers #1

--- gulp/tasks/concat.js
+++ gulp/tasks/concat.js
@@ -10,19 +10,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
-*/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const strip = require('gulp-strip-comments');
+const header = require('gulp-header');
+const fs = require('fs');
+
 const config = require('../config.js').concat;
 
 gulp.task('concat', () => {
   return gulp.src(config.src)
+             .pipe(strip())
              .pipe(concat(config.output))
+             .pipe(header(fs.readFileSync('gulp/license-header.txt', 'utf8')))
              .pipe(gulp.dest(config.dest));
 });
